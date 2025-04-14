2a19f892c6a166b729d74b01efd3068b7e4e9c63
tr-nam <tr.namm29@gmail.com>
2025-04-15 00:21:21 +0700

    base header/config router

---
+++ src/assets/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import logo__light from './logo_no_bg_light.jpg';
+import logo__dark from './logo_no_bg.jpg';
+
+const index = {
+    logo__light,
+    logo__dark
+}
+
+export default index
---
+++ src/layouts/Header/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { NavLink } from 'react-router';
+
+import assets from '@/assets';//import lấy ảnh
+import InputSearch from '@/components/InputSearch';
+
+const Header = () => {
+    const navigation = [
+        {
+            name: "Home",
+            link: "/"
+        },
+        {
+            name: "Map",
+            link: "/map"
+        },
+    ]
+
+    return (
+        <nav className="sticky top-0 z-50 bg-white/90 backdrop-blur-md border-b shadow-sm">
+            <div className="container mx-auto flex justify-between items-center py-4">
+                <div className="w-3xs h-12 flex justify-center items-center">
+                    <img src={assets.logo__dark} alt="logo" className='bg-cover w-1/2' />
+                </div>
+                <div className='w-fit flex items-center'>
+                    {navigation.map((item) => (
+                        <NavLink to={item.link} key={item.link} className={`font-bold! text-xl! px-1.5 text-gray-700! hover:text-gray-950!`}>
+                            {item.name}
+                        </NavLink>
+                    ))}
+                    <InputSearch className="ml-5"/>
+                </div>
+            </div>
+        </nav >
+    )
+}
+
+export default Header
